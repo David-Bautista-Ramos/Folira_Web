7dd30c8641edb9e28dc784ea6e1631d0c4b0467c
Catacar2006 <catalinacardenas459@gmail.com>
2024-11-11 13:30:07 -0500

    Se hizo algo, pero se debe corregir las fichas tecnicas

--- folira/src/pages/libro/FichaLibro.jsx
+++ folira/src/pages/libro/FichaLibro.jsx
@@ -211,186 +211,186 @@ if (isRedirecting) {
     : resenas;
 
   return (
-    <div className='flex-[4_4_0] border-r border-primary min-h-screen '> 
-      <div className="flex flex-col p-6 bg-white rounded-lg shadow-lg relative">
-        {/* Render del libro */}
-        <div className="flex items-center cursor-pointer gap-5 text-3xl -mt-4 border-b-2 border-gray-300 pb-2 mb-4" onClick={handleRedirect}>
-          <Link to="/libro"> 
-            <BsArrowLeft className="text-primary mr-2 text-lg " /> {/* Icono de flecha */}
-          </Link>
-            <span className="text-xl text-primary font-bold flex items-center">{titulo}</span> {/* Título del libro */}
-        </div>
+    <div className="flex-[4_4_0] border-r border-primary min-h-screen">
+  <div className="flex flex-col p-6 bg-white rounded-lg shadow-lg relative">
+    {/* Encabezado con flecha y título */}
+    <div
+      className="flex items-center cursor-pointer gap-5 text-3xl -mt-4 border-b-2 border-gray-300 pb-2 mb-4"
+      onClick={handleRedirect}
+    >
+      <Link to="/libro">
+        <BsArrowLeft className="text-primary mr-2 text-lg" />
+      </Link>
+      <span className="text-xl text-primary font-bold flex items-center">
+        {titulo}
+      </span>
+    </div>
 
-        <div className="flex">
-          <img 
-            src={portada || "/book-placeholder.png"} 
-            alt={titulo} 
-            className="w-1/3 h-[auto] rounded-lg object-cover"
-            style={{
-              flexShrink: 0,
-              maxHeight: '300px',  // Ajusta la altura máxima de la imagen
-              objectFit: 'cover',  // Mantiene la proporción sin distorsionar la imagen
-            }}
-          />
-          <div className="ml-6 flex flex-col flex-grow">
-            <h2 className="text-2xl font-semibold">{titulo}</h2>
-            <p className="text-lg font-medium"><strong>Autor:</strong> {autores.length > 0 ? autores.map((autor) => autor.nombre).join(', '): 'Autor no disponible'}</p>
-            <p className="text-md"><strong>Serie:</strong> {serie || 'N/A'}</p>
-            <p className="text-md"><strong>ISBN:</strong> {isbn}</p>
-            <div className="text-md break-all">
-              {renderSinopsis()}
-            </div>
-            <div className="flex items-center mt-2">
-              <strong>Calificación General:</strong> 
-              {renderEstrellas(calificacionPromedio)} {/* Mostrar calificación promedio en estrellas */}
-            </div> 
-            <strong>Generos literarios:</strong>
-            <div className='flex flex-wrap gap-4'>
-            {generos.length > 0 ? (
-              generos.map((genero) => (
-                <div 
-                  key={genero.nombre} 
-                  className='flex items-center border rounded-full p-2 bg-white min-w-[120px] max-w-[150px] truncate'
-                >
-                  <img src={genero.fotoGenero} alt={genero.nombre} className='w-8 h-8 mr-2' />
-                  <span className="truncate">{genero.nombre}</span>
-                </div>
-              ))
-            ) : (
-              <span>No tiene géneros asignados</span> // Mensaje cuando no hay géneros
-            )}
-          </div>
-          </div>
-        </div>
-
+    <div className="flex">
+      {/* Primera columna: Imagen, botón, estrellas */}
+      <div className="w-1/3 flex flex-col items-center">
+        <img
+          src={portada || "/book-placeholder.png"}
+          alt={titulo}
+          className="w-full rounded-lg object-cover"
+          style={{ maxHeight: '300px', objectFit: 'cover' }}
+        />
         <button
-          className="mt-4 bg-primary hover:bg-blue-950 text-white font-bold py-2 w-[180px] px-4 rounded"
+          className="mt-4 bg-primary hover:bg-blue-950 text-white font-bold py-2 px-4 rounded"
           onClick={openModal}
         >
           Ver reseñas
         </button>
-
-        <form onSubmit={handleSubmit} className="mt-4 w-full">
-        <div className="flex ml-2">{renderEstrellas(calificacion)}</div>
-          <label className="block text-md font-medium mb-2">Escribe una reseña:</label>
-          <div className="flex w-full">
-            <input
-              type="text"
-              value={comentario}
-              onChange={(e) => setComentario(e.target.value)}
-              className="w-full p-2 border rounded focus:border-primary focus:outline-none"
-              placeholder="Escribe tu reseña..."
-              aria-label="Escribe tu reseña"
-            />
-            <button
-              type="submit"
-              className="ml-2 bg-primary hover:bg-blue-950 text-white p-2 rounded-lg"
-            >
-              Enviar
-            </button>
-          </div>
-        </form>
-
-        {/* Renderizar solo las primeras 5 reseñas */}
-       <div className="mt-6">
-  {resenas.length === 0 ? ( // Comprobar si no hay reseñas
-    <p>No tiene reseñas.</p>
-  ) : (
-    // Asegúrate de invertir el arreglo para que la reseña más reciente esté en la parte superior
-    resenas.slice().reverse().slice(0, 5).map((reseña, index) => (
-      <div key={index} className="flex items-start mb-4 p-4 border border-gray-200 rounded-lg bg-gray-100 break-all">
-        <img 
-          src={reseña.idUsuario.fotoPerfil || 'https://via.placeholder.com/48'} 
-          alt={`${reseña.idUsuario.nombre} perfil`} 
-          className="w-12 h-12 rounded-full mr-4"
-          style={{ width: '48px', height: '48px' }} // Ajustar tamaño a 48x48
-        />
-        <div>
-          <h3 className="font-semibold">{reseña.idUsuario.nombre}</h3>
-          <p className="text-md">{reseña.contenido}</p>
-        </div>
-        <div className="flex mt-1">
-          {renderEstrellasCom(reseña.calificacion)} {/* Renderización de estrellas */}
-        </div>
+        <div className="mt-2">{renderEstrellas(calificacionPromedio)}</div>
       </div>
-    ))
-  )}
-</div>
 
-
-
-
-{isModalOpen && (
-          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 break-all">
-          <div className="bg-white rounded-lg p-4 w-[40%] max-h-[800px] overflow-hidden"> {/* Aumenté el max-h a 800px */}
-            <h3 className="text-lg font-semibold mb-2">Reseñas</h3>
-        
-            <div className="mb-2">
-              <h4 className="text-sm font-medium">Filtrar por calificación:</h4>
-              <div className="flex">
-                {[1, 2, 3, 4, 5].map((star) => (
-                  <div
-                    key={star}
-                    onClick={() => setSelectedStarFilter(star)}
-                    className={`cursor-pointer ${selectedStarFilter === star ? 'text-yellow-500' : 'text-gray-300'}`}
-                  >
-                    {star <= selectedStarFilter ? <BsStarFill /> : <BsStar />}
-                  </div>
-                ))}
-                <div
-                  onClick={() => setSelectedStarFilter(0)}
-                  className={`ml-2 cursor-pointer ${selectedStarFilter === 0 ? 'text-yellow-500' : 'text-gray-300'}`}
-                >
-                  <span>Todo</span>
-                </div>
-              </div>
-            </div>
-        
-            <div className="modal-container" style={{ maxHeight: '400px', overflowY: 'auto', scrollbarWidth: 'thin', scrollbarColor: '#111827 transparent' }}> {/* Aumenté maxHeight a 400px */}
-              {loadingReseñas ? (
-                <p>Cargando reseñas...</p>
-              ) : (
-                filteredReseñas.length > 0 ? (
-                  filteredReseñas.slice().reverse().map((reseña) => (
-                    <div key={reseña._id} className="flex items-start mb-2 p-2 border border-gray-200 rounded-lg bg-gray-100">
-                      <img 
-                        src={reseña.idUsuario.fotoPerfil || 'https://via.placeholder.com/48'} 
-                        alt={`${reseña.idUsuario.nombre} perfil`} 
-                        className="w-10 h-10 rounded-full mr-2" 
-                      />
-                      <div className="flex-grow">
-                        <h3 className="font-semibold text-sm">{reseña.idUsuario.nombre}</h3>
-                        <p className="text-sm break-all">{reseña.contenido}</p>
-                        <div className="flex mt-1">
-                          {renderEstrellasCom(reseña.calificacion)}
-                        </div>
-                      </div>
-                      {reseña.idUsuario._id === authUser._id && (
-                        <button 
-                          onClick={() => handleDeleteReseña(reseña._id)}
-                          className="ml-2 text-red-500 hover:text-red-700"
-                        >
-                          <FaTrash className="text-primary cursor-pointer hover:text-blue-900" />
-                        </button>
-                      )}
-                    </div>
-                  ))
-                ) : (
-                  <p>No hay reseñas para mostrar.</p>
-                )
-              )}
-            </div>
-            
-            <button 
-              onClick={closeModal}       
-              className="mb-2 mt-4 bg-primary hover:bg-blue-950 text-white px-4 py-2 rounded ml-[85%]"> {/* Asegurando que el botón ocupe todo el ancho */}
-              Cerrar
-            </button>
-          </div>
+      {/* Segunda columna: Detalles del libro */}
+      <div className="ml-6 flex flex-col flex-grow">
+        <h2 className="text-2xl font-semibold">{titulo}</h2>
+        <p className="text-lg font-medium">
+          <strong>Autor:</strong>{" "}
+          {autores.length > 0
+            ? autores.map((autor) => autor.nombre).join(", ")
+            : "Autor no disponible"}
+        </p>
+        <p className="text-md">
+          <strong>Serie:</strong> {serie || "N/A"}
+        </p>
+        <p className="text-md">
+          <strong>ISBN:</strong> {isbn}
+        </p>
+        <div className="text-md break-all">{renderSinopsis()}</div>
+        <div className="flex items-center mt-2">
+          <strong>Calificación General:</strong>{" "}
+          {renderEstrellas(calificacionPromedio)}
         </div>
-        )}
+        <strong>Géneros Literarios:</strong>
+        <div className="flex flex-wrap gap-4">
+          {generos.length > 0 ? (
+            generos.map((genero) => (
+              <div
+                key={genero.nombre}
+                className="flex items-center border rounded-full p-2 bg-white min-w-[120px] max-w-[150px] truncate"
+              >
+                <img
+                  src={genero.fotoGenero}
+                  alt={genero.nombre}
+                  className="w-8 h-8 mr-2"
+                />
+                <span className="truncate">{genero.nombre}</span>
+              </div>
+            ))
+          ) : (
+            <span>No tiene géneros asignados</span>
+          )}
+        </div>
       </div>
     </div>
+
+    {/* Formulario para escribir reseña */}
+    <form onSubmit={handleSubmit} className="mt-4 w-full">
+      <div className="flex ml-2">{renderEstrellas(calificacion)}</div>
+      <label className="block text-md font-medium mb-2">Escribe una reseña:</label>
+      <div className="flex w-full">
+        <input
+          type="text"
+          value={comentario}
+          onChange={(e) => setComentario(e.target.value)}
+          className="w-full p-2 border rounded focus:border-primary focus:outline-none"
+          placeholder="Escribe tu reseña..."
+          aria-label="Escribe tu reseña"
+        />
+        <button
+          type="submit"
+          className="ml-2 bg-primary hover:bg-blue-950 text-white p-2 rounded-lg"
+        >
+          Enviar
+        </button>
+      </div>
+    </form>
+
+    {/* Reseñas */}
+    <div className="mt-6">
+      {resenas.length === 0 ? (
+        <p>No tiene reseñas.</p>
+      ) : (
+        resenas
+          .slice()
+          .reverse()
+          .slice(0, 5)
+          .map((reseña, index) => (
+            <div
+              key={index}
+              className="flex items-start mb-4 p-4 border border-gray-200 rounded-lg bg-gray-100 break-all"
+            >
+              <img
+                src={reseña.idUsuario.fotoPerfil || "https://via.placeholder.com/48"}
+                alt={`${reseña.idUsuario.nombre} perfil`}
+                className="w-12 h-12 rounded-full mr-4"
+                style={{ width: "48px", height: "48px" }}
+              />
+              <div>
+                <h3 className="font-semibold">{reseña.idUsuario.nombre}</h3>
+                <p className="text-md">{reseña.contenido}</p>
+              </div>
+              <div className="flex mt-1">
+                {renderEstrellasCom(reseña.calificacion)}
+              </div>
+            </div>
+          ))
+      )}
+    </div>
+
+    {isModalOpen && (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 break-all">
+        <div className="bg-white rounded-lg p-4 w-[40%] max-h-[800px] overflow-hidden">
+          <h3 className="text-lg font-semibold mb-2">Reseñas</h3>
+
+          {/* Modal content */}
+          <div className="modal-container" style={{ maxHeight: "400px", overflowY: "auto" }}>
+            {loadingReseñas ? (
+              <p>Cargando reseñas...</p>
+            ) : filteredReseñas.length > 0 ? (
+              filteredReseñas.slice().reverse().map((reseña) => (
+                <div key={reseña._id} className="flex items-start mb-2 p-2 border border-gray-200 rounded-lg bg-gray-100">
+                  <img
+                    src={reseña.idUsuario.fotoPerfil || "https://via.placeholder.com/48"}
+                    alt={`${reseña.idUsuario.nombre} perfil`}
+                    className="w-10 h-10 rounded-full mr-2"
+                  />
+                  <div className="flex-grow">
+                    <h3 className="font-semibold text-sm">{reseña.idUsuario.nombre}</h3>
+                    <p className="text-sm break-all">{reseña.contenido}</p>
+                    <div className="flex mt-1">
+                      {renderEstrellasCom(reseña.calificacion)}
+                    </div>
+                  </div>
+                  {reseña.idUsuario._id === authUser._id && (
+                    <button
+                      onClick={() => handleDeleteReseña(reseña._id)}
+                      className="ml-2 text-red-500 hover:text-red-700"
+                    >
+                      <FaTrash className="text-primary cursor-pointer hover:text-blue-900" />
+                    </button>
+                  )}
+                </div>
+              ))
+            ) : (
+              <p>No hay reseñas para mostrar.</p>
+            )}
+          </div>
+          <button
+            onClick={closeModal}
+            className="mb-2 mt-4 bg-primary hover:bg-blue-950 text-white px-4 py-2 rounded ml-[85%]"
+          >
+            Cerrar
+          </button>
+        </div>
+      </div>
+    )}
+  </div>
+</div>
+
     
   );
 };
